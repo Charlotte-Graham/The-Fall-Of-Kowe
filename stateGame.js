a9064da28a12c4bd92497c9895487fdaf5c8a8c2
Audrey Kay <audrey.kay@live.com.au>
2015-06-15 16:13:30 +1000

    tidied html file, added game state

---
+++ stateGame.js
@@ -0,0 +1,53 @@
+var GameState = function() 
+{
+	this.prototype = BaseState;
+}
+
+GameState.prototype.load = function() 
+{
+	
+}
+
+GameState.prototype.unload = function() 
+{
+	
+}
+
+GameState.prototype.update = function(dt) 
+{
+	
+}
+
+GameState.prototype.draw = function() 
+{
+	// Paint canvas black.
+	context.fillStyle = "black";
+	context.rect(0, 0, canvas.width, canvas.height);
+	context.fill();
+	
+	function stars() 
+	{
+		// Draw 50 stars.
+		for (i = 0; i <= 50; i++) 
+		{
+		// Get random positions for stars.
+		var x = Math.floor(Math.random() * canvas.width)
+		var y = Math.floor(Math.random() * canvas.height)
+		
+		// star colour
+		context.fillStyle = "yellow";
+
+		// Give the player some room.
+		// if (x < 0 || y < 0) ctx.fillStyle = "black";
+
+		// Draw an individual star.
+		context.beginPath();
+		context.arc(x, y, 3, 0, Math.PI * 2, true);
+		context.closePath();
+		context.fill();
+		}
+	}
+	
+	// Paint the starfield.
+	stars();
+}
