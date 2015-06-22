a4d7d9fcfd9946450c6546b25aceaf63370f1dd5
The Ewok <princessewok@hotmail.com>
2015-06-22 21:03:14 +1000

    hopefully final change

--- debris.js
+++ debris.js
@@ -1,4 +1,3 @@
-var spawnTimer = 0;
 var DEBRIS_SPEED = 0.8;
 var debrisArray = [];
 
@@ -7,74 +6,42 @@ function rand(floor, ceil)
     return Math.floor( (Math.random()* (ceil-floor)) +floor );
 }
 
-var Debris = function()
+function spawnDebris()
 {
-    function spawnDebris()
+    var type = rand(0, 3);
+   
+    var debris = {};
+    
+    debris.image = document.createElement("img");
+    debris.image.src = "over.png";
+    debris.width = 32;
+    debris.height = 32;
+    
+    var x = SCREEN_WIDTH/2;
+    var y = SCREEN_HEIGHT/2;
+    
+    var dirX = rand(-10, 10);
+    var dirY = rand(-10, 10);
+    
+    var magnitude = (dirX * dirX) + (dirY * dirY);
+    if(magnitude != 0)
     {
-        var type = rand(0, 3);
-       
-        var debris = {};
-        
-        debris.image = document.createElement("img");
-        debris.image.src = "over.png";
-        debris.width = 32;
-        debris.height = 32;
-        
-        var x = SCREEN_WIDTH/2;
-        var y = SCREEN_HEIGHT/2;
-        
-        var dirX = rand(-10, 10);
-        var dirY = rand(-10, 10);
-        
-        var magnitude = (dirX * dirX) + (dirY * dirY);
-        if(magnitude != 0)
-        {
-            var oneOverMag = 1 / Math.sqrt(magnitude);
-            dirX *= oneOverMag;
-            dirY *= oneOverMag;
-        }
-        
-        var movX = dirX * SCREEN_WIDTH;
-        var movY = dirY * SCREEN_HEIGHT;
-        
-        debris.x = x + movX;
-        debris.y = y + movY;
-        
-        debris.velocityX = -dirX * DEBRIS_SPEED;
-        debris.velocityY = -dirY * DEBRIS_SPEED;
-        
-        debris.push(debrisArray);
+        var oneOverMag = 1 / Math.sqrt(magnitude);
+        dirX *= oneOverMag;
+        dirY *= oneOverMag;
     }
-
-    /*this.image = document.createElement("img");
-    this.position = new Vector2();
-    this.position.set();
-
-    this.width = 32;
-    this.height = 32;
-
-    this.offset = new Vector2();
-    this.offset.set(-55, -87);
-
-    this.velocity = new Vector2();
-
-    this.image.src = "over.png";*/
+    
+    var movX = dirX * SCREEN_WIDTH;
+    var movY = dirY * SCREEN_HEIGHT;
+    
+    debris.x = x + movX;
+    debris.y = y + movY;
+    
+    debris.velocityX = -dirX * DEBRIS_SPEED;
+    debris.velocityY = -dirY * DEBRIS_SPEED;
+    
+    debris.push(debrisArray);
 }
-
-Debris.prototype.draw = function()
-{
-    for(var i=0; i<debris.length; i++)
-    {
-        context.drawImage(debris[i].image, debris[i].x, debris[i].y);
-    }
-        
-    spawnTimer -= deltaTime;
-    if(spawnTimer <= 0)
-    {
-        spawnTimer = 1;
-        spawnDebris();
-    }
-
     /*for(var i=0; i<debris.length; i++)
     {
         for(var j=0; j<player.length; j++)
@@ -91,4 +58,3 @@ Debris.prototype.draw = function()
             }
         }
     }*/
-}
