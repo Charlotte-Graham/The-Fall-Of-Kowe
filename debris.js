b88dde09fb34db1dbece4e882144703f5165b350
The Ewok <princessewok@hotmail.com>
2015-06-22 20:23:49 +1000

    added debris picture

--- debris.js
+++ debris.js
@@ -1,39 +1,58 @@
 var spawnTimer = 0;
-function spawnDebris()
+var ASTEROID_SPEED = 0.8;
+
+var Debris = function()
 {
-    var type = rand(0, 3);
-   
-    var debris = {};
-    
-    debris.image = document.createElement("img");
-    debris.image.src = "over.png";
-    debris.width = 69;
-    debris.height = 75;
-    
-    var x = SCREEN_WIDTH/2;
-    var y = SCREEN_HEIGHT/2;
-    
-    var dirX = rand(-10, 10);
-    var dirY = rand(-10, 10);
-    
-    var magnitude = (dirX * dirX) + (dirY * dirY);
-    if(magnitude != 0)
+    function spawnDebris()
     {
-        var oneOverMag = 1 / Math.sqrt(magnitude);
-        dirX *= oneOverMag;
-        dirY *= oneOverMag;
+        var type = rand(0, 3);
+       
+        var debris = {};
+        
+        debris.image = document.createElement("img");
+        debris.image.src = "over.png";
+        debris.width = 69;
+        debris.height = 75;
+        
+        var x = SCREEN_WIDTH/2;
+        var y = SCREEN_HEIGHT/2;
+        
+        var dirX = rand(-10, 10);
+        var dirY = rand(-10, 10);
+        
+        var magnitude = (dirX * dirX) + (dirY * dirY);
+        if(magnitude != 0)
+        {
+            var oneOverMag = 1 / Math.sqrt(magnitude);
+            dirX *= oneOverMag;
+            dirY *= oneOverMag;
+        }
+        
+        var movX = dirX * SCREEN_WIDTH;
+        var movY = dirY * SCREEN_HEIGHT;
+        
+        debris.x = x + movX;
+        debris.y = y + movY;
+        
+        debris.velocityX = -dirX * DEBRIS_SPEED;
+        debris.velocityY = -dirY * DEBRIS_SPEED;
+        
+        debris.push(debris);
     }
-    
-    var movX = dirX * SCREEN_WIDTH;
-    var movY = dirY * SCREEN_HEIGHT;
-    
-    debris.x = x + movX;
-    debris.y = y + movY;
-    
-    debris.velocityX = -dirX * DEBRIS_SPEED;
-    debris.velocityY = -dirY * DEBRIS_SPEED;
-    
-    debris.push(debris);
+
+    /*this.image = document.createElement("img");
+    this.position = new Vector2();
+    this.position.set();
+
+    this.width = 32;
+    this.height = 32;
+
+    this.offset = new Vector2();
+    this.offset.set(-55, -87);
+
+    this.velocity = new Vector2();
+
+    this.image.src = "over.png";*/
 }
 
 Debris.prototype.draw = function()
