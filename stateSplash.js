03b3aced94b3ecd14dde590218a2812c7a47b4e6
The Ewok <princessewok@hotmail.com>
2015-06-22 19:41:53 +1000

    updated so that instead of text it shows image

--- stateSplash.js
+++ stateSplash.js
@@ -13,6 +13,15 @@ SplashState.prototype.unload = function()
 	
 }
 
+var splashScreen = 
+{
+	image: document.createElement("img"),
+	width: 640,
+	height: 525
+};
+
+splashScreen.image.src = "splashScreen.png";
+
 var splashTimer = 3;
 SplashState.prototype.update = function(dt) 
 {
@@ -29,18 +38,8 @@ SplashState.prototype.update = function(dt)
 
 SplashState.prototype.draw = function() 
 {
-	context.fillStyle = "#FF6600";
-	context.font="76px Hobo Std";
-	var width = context.measureText("Fall of Kowe").width;
-	context.fillText("Fall of Kowe", canvas.width/2 - width/2, canvas.height/2);
-
-	context.fillStyle = "#FF6600";
-	context.font="34px Hobo Std";
-	var width = context.measureText("Controls").width;
-	context.fillText("Controls", SCREEN_WIDTH - 400, 300, canvas.width/2 - width/2, canvas.height/2);
-
-	context.fillStyle = "#FF6600";
-	context.font="34px Hobo Std";
-	var width = context.measureText("Left/Right Arrow Keys").width;
-	context.fillText("Left/Right Arrow Keys", SCREEN_WIDTH - 405, 350, canvas.width/2 - width/2, canvas.height/2);
+	context.fillStyle = "#ccc";
+	context.fillRect(0, 0, canvas.width, canvas.height);
+	
+	context.drawImage(splashScreen.image, 0, 0);
 }
