40c9bf25f8d599f9c8daa57e95a58d8739d4aa88
The Ewok <princessewok@hotmail.com>
2015-07-01 20:46:07 +1000

    everything finished

---
+++ Game/powerup.js
@@ -0,0 +1,50 @@
+var canvas = document.getElementById("gameCanvas");
+
+var SCREEN_WIDTH = canvas.width;
+var SCREEN_HEIGHT = canvas.height;
+
+var POWERUP_SPEED = 0.8;
+var powerupArray = [];
+
+function rand(floor, ceil)
+{
+    return Math.floor( (Math.random()* (ceil-floor)) +floor );
+}
+
+function spawnPowerup()
+{
+    var type = rand(0, 3);
+   
+    var powerup = {};
+    
+    powerup.image = document.createElement("img");
+    powerup.image.src = "blueCoinPlus.png";
+    powerup.width = 32;
+    powerup.height = 32;
+    
+    var x = SCREEN_WIDTH/2;
+    var y = SCREEN_HEIGHT/2;
+    
+    var dirX = rand(-10, 10);
+    var dirY = rand(-10, 10);
+    
+    var magnitude = (dirX * dirX) + (dirY * dirY);
+    if(magnitude != 0)
+    {
+        var oneOverMag = 1 / Math.sqrt(magnitude);
+        dirX *= oneOverMag;
+        dirY *= oneOverMag;
+    }
+    
+    var movX = dirX * SCREEN_WIDTH;
+    var movY = dirY * SCREEN_HEIGHT;
+    
+    powerup.x = x + movX;
+    powerup.y = y + movY;
+    
+    powerup.velocityX = -dirX * POWERUP_SPEED;
+    powerup.velocityY = -dirY * POWERUP_SPEED;
+    
+    powerupArray.push(powerup);
+}
+
---
+++ Old Stuff/stateOver.js
@@ -0,0 +1,64 @@
+var OverState = function() 
+{
+	this.prototype = BaseState;
+}
+
+OverState.prototype.load = function() 
+{
+	
+}
+
+OverState.prototype.unload = function() 
+{
+	
+}
+
+OverState.prototype.update = function(dt) 
+{
+	context.fillStyle = "#003300";
+	context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+OverState.prototype.draw = function() 
+{
+	// Paint canvas black.
+	context.fillStyle = "black";
+	context.rect(0, 0, canvas.width, canvas.height);
+	context.fill();
+	
+	function stars() 
+	{
+		// Draw 50 stars.
+		for (i = 0; i <= 50; i++) 
+		{
+		// Get random positions for stars.
+		var x = Math.floor(Math.random() * canvas.width)
+		var y = Math.floor(Math.random() * canvas.height)
+		
+		// star colour
+		context.fillStyle = "yellow";
+
+		// Give the player some room.
+		// if (x < 0 || y < 0) ctx.fillStyle = "black";
+
+		// Draw an individual star.
+		context.beginPath();
+		context.arc(x, y, 3, 0, Math.PI * 2, true);
+		context.closePath();
+		context.fill();
+		}
+	}
+	
+	// Paint the starfield.
+	stars();
+	//draw the score
+	//, 100
+	context.fillStyle = "#f00";
+	context.font="34px Hobo Std";
+	context.fillText("Score: " + score, 230, 280);
+	//GAME OVER!!!!!!!!!!!!!!!!!!!!
+	context.fillStyle = "#ff6c00";
+	context.font="76px Hobo Std";
+	var width = context.measureText("Game Over!").width;
+	context.fillText("Game Over!", canvas.width/2 - width/2, canvas.height/2);
+}
